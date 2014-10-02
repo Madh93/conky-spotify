44a536faf3afdf696bd5ce9132bdbd5f8ebda3f8
Madh93 <michelangelo93@gmail.com>
2014-10-02 19:06:31 +0100

    Fixing ID track

--- scripts/imgurl.sh
+++ scripts/imgurl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-imgurl=`curl -X GET https://api.spotify.com/v1/tracks/$id_nuevo | grep '"url" : ' | sed '2q;d' | cut -d '"' -f4`
+imgurl=`curl -X GET https://api.spotify.com/v1/tracks/$1 | grep '"url" : ' | sed '2q;d' | cut -d '"' -f4`
 echo $imgurl
