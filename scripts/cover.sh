52c7cf8bada5e80c24518c310dbf2d64d09924f1
Madh93 <michelangelo93@gmail.com>
2014-10-02 18:44:20 +0100

    Getting current cover

---
+++ scripts/cover.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+id_current=`cat ~/.conky/conky-spotify/scripts/current_cover`
+id_new=`~/.conky/conky-spotify/scripts/id.sh`
+cover=
+imgurl=
+
+if [ "$id_new" != "$id_current" ]; then
+
+	cover=`ls ~/.conky/conky-spotify/covers | grep $id_new`
+
+	if [ "$cover" == "" ]; then
+
+		imgurl=`~/.conky/conky-spotify/scripts/imgurl.sh`
+		wget -O ~/.conky/conky-spotify/scovers/$id_new.jpg $imgurl
+		cover=`ls ~/.conky/conky-spotify/covers | grep $id_new`
+	fi
+
+	if [ "$cover" != "" ]; then
+		cp ~/.conky/conky-spotify/covers/$cover ~/.conky/conky-spotify/covers/current.jpg
+	else
+		cp ~/.conky/conky-spotify/covers/empty.jpg ~/.conky/conky-spotify/covers/current.jpg
+	fi
+fi	
