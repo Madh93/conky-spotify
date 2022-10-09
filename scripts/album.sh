d8d9e50eb9e72d40bad174b29a3d2e68eac64a5d
BarkleyIII <yannik@blaviken.localhost>
2022-10-09 17:43:26 +0200

    replace egrep with grep -E
    The egrep and fgrep commands have been deprecated since grep 2.5.3 (in 2007). Since the last release (3.8), they print a warning that grep -E and grep -F should be used instead.

--- scripts/album.sh
+++ scripts/album.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
  
-album=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 1 "album"|egrep "^\s*variant"|cut -b 44-|egrep -v ^$|sed 's/"$//'`
+album=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|grep -E -A 1 "album"|grep -E "^\s*variant"|cut -b 44-|grep -E -v ^$|sed 's/"$//'`
 echo $album
--- scripts/imgurl.sh
+++ scripts/imgurl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-trackid=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | egrep -A 1 "trackid" | egrep -v "trackid"| cut -b 44- | cut -d '"' -f 1`
+trackid=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep -E -A 1 "trackid" | grep -E -v "trackid"| cut -b 44- | cut -d '"' -f 1`
 #echo $trackid
 oembed_url="https://open.spotify.com/oembed?url=$trackid"
 #echo $oembed_url
