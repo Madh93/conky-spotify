bb0f7388b67e6e2ca7f5f8b7568b671fb99334f8
lipebol <lipebol@outlook.com>
2023-05-16 12:38:58 -0300

    Update indentation

--- scripts/cover.sh
+++ scripts/cover.sh
@@ -7,7 +7,7 @@ imgurl=
 dbus=`busctl --user list | grep "spotify"`
 
 if [ "$dbus" == "" ]; then
-    `cp ~/.conky/conky-spotify/empty.jpg ~/.conky/conky-spotify/current/current.jpg`
+       `cp ~/.conky/conky-spotify/empty.jpg ~/.conky/conky-spotify/current/current.jpg`
 	echo "" > ~/.conky/conky-spotify/current/current.txt
 else
 	if [ "$id_new" != "$id_current" ]; then
@@ -22,11 +22,11 @@ else
 			`cp ~/.conky/conky-spotify/covers/$imgname.jpg ~/.conky/conky-spotify/current/current.jpg`
 		else
 			imgurl=`~/.conky/conky-spotify/scripts/imgurl.sh`
-        	`wget -q -O ~/.conky/conky-spotify/covers/$imgname.jpg $imgurl &> /dev/null`
+			`wget -q -O ~/.conky/conky-spotify/covers/$imgname.jpg $imgurl &> /dev/null`
 			`cp ~/.conky/conky-spotify/covers/$imgname.jpg ~/.conky/conky-spotify/current/current.jpg`
 			# clean up covers folder, keeping only the latest X amount, in below example it is 10
-	    	rm -f `ls -t ~/.conky/conky-spotify/covers/* | awk 'NR>10'`
-	    	rm -f wget-log #wget-logs are accumulated otherwise
+			rm -f `ls -t ~/.conky/conky-spotify/covers/* | awk 'NR>10'`
+			rm -f wget-log #wget-logs are accumulated otherwise
 		fi
 	fi
 fi
